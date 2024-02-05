f62499c0462ac63883fb6158dd06fd7dd88b1d7c
Bilal <bilalbakhrom@gmail.com>
2024-02-05 19:09:41 +0500

    feat: update demo project

--- SoundControlKitExample/SoundControlKitExample/Views/AudioManagerView.swift
+++ SoundControlKitExample/SoundControlKitExample/Views/AudioManagerView.swift
@@ -90,6 +90,13 @@ struct AudioManagerView: View {
             .onAppear {
                 viewModel.prepare()
             }
+            .alert(isPresented: $viewModel.isPermissionAlertPresented) {
+                Alert(
+                    title: Text("Microphone Access Required"),
+                    message: Text("Please enable microphone access in Settings to use this feature."),
+                    dismissButton: .cancel()
+                )
+            }
         }
     }
 }
@@ -97,4 +104,25 @@ struct AudioManagerView: View {
 #Preview {
     AudioManagerView()
         .preferredColorScheme(.dark)
+}
+
+/// Method to show an alert if the user hasn't granted microphone permission
+func showMicrophonePermissionAlert(on viewController: UIViewController) {
+    let alertController = UIAlertController(
+        title: "Microphone Access Required",
+        message: "Please enable microphone access in Settings to use this feature.",
+        preferredStyle: .alert
+    )
+    
+    alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+    
+    alertController.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
+        if let settingsURL = URL(string: UIApplication.openSettingsURLString) {
+            UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+        }
+    }))
+    
+    DispatchQueue.main.async {
+        viewController.present(alertController, animated: true)
+    }
 }
